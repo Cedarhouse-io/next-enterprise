e64361d5ac73d60dc909e8596ffa577f178f528f
DerilB <128557270+DerilB@users.noreply.github.com>
2024-04-15 12:43:29 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,19 +5,23 @@ import { env } from "./env.mjs";
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
-  basePath: '/next-enterprise',  // Replace [repository-name] with your actual repository name
-  output: 'standalone',
-  reactStrictMode: true,
-  experimental: { instrumentationHook: true },
-  rewrites() {
-    return [
-      { source: "/healthz", destination: "/api/health" },
-      { source: "/api/healthz", destination: "/api/health" },
-      { source: "/health", destination: "/api/health" },
-      { source: "/ping", destination: "/api/health" },
-    ];
-  },
-});
+const config = withPlugins(
+  [[withBundleAnalyzer({ enabled: env.ANALYZE })]],
+  {
+    reactStrictMode: true,
+    experimental: { instrumentationHook: true },
+    basePath: '/next-enterprise',  // Replace [repository-name] with your actual repository name
+    assetPrefix: '/next-enterprise/',  // Replace [repository-name] with your actual repository name
+    output: 'standalone',
+    rewrites() {
+      return [
+        { source: "/healthz", destination: "/api/health" },
+        { source: "/api/healthz", destination: "/api/health" },
+        { source: "/health", destination: "/api/health" },
+        { source: "/ping", destination: "/api/health" },
+      ];
+    },
+  }
+);
 
 export default config;
